14f053da14fc5a296c831f6e1622316f0c767dcb
Lucas <lucas.negritto@gmail.com>
2021-10-25 00:33:38 -0700

    fix

--- gpt.py
+++ gpt.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -6,20 +6,22 @@ app = FastAPI()
 
 class Output(BaseModel):
     result: str
-  
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
+
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 
 @app.post("/")
 async def root(output: Output):
     print(output)
     prompt = "In a shocking finding, scientists discovered a herd of unicorns living in a remote, " \
-    "previously unexplored valley, in the Andes Mountains. Even more surprising to the " \
-    "researchers was the fact that the unicorns spoke perfect English."
+         "previously unexplored valley, in the Andes Mountains. Even more surprising to the " \
+         "researchers was the fact that the unicorns spoke perfect English."
 
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
     gen_tokens = model.generate(input_ids, do_sample=True, temperature=0.9, max_length=100,)
     gen_text = tokenizer.batch_decode(gen_tokens)[0]
+
     print(gen_text)
     return {"message": gen_text}
